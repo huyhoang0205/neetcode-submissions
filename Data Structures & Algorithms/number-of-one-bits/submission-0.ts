91de7aa6a09bdbe5a3702b0f3d53df920de25491
Nguyễn Đình Huy Hoàng <101924758+huyhoang0205@users.noreply.github.com>
2026-09-21 19:53:41 +0700

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.ts
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number}
+     */
+    hammingWeight(n: number): number {
+        let count = 0;
+        for(let i = 0; i < 32 ; i++) {
+            if (n & (2 ** i)) count++;
+        }
+
+        return count;
+    }
+}
