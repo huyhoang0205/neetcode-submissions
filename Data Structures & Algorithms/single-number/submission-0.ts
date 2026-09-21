34ff9b763431a0e53b198170b13f2fb451fdc636
Nguyễn Đình Huy Hoàng <101924758+huyhoang0205@users.noreply.github.com>
2026-09-21 16:23:11 +0700

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.ts
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums: number[]): number {
+        let res = nums[0]
+        for(let i = 1 ; i < nums.length; i++ ) {
+            res = res ^ nums[i];
+        }
+
+        return res;
+    }
+}
