f9cadf38108b7fa70ce34591e7057c3781a25f86
Nguyễn Đình Huy Hoàng <101924758+huyhoang0205@users.noreply.github.com>
2026-09-21 20:35:45 +0700

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number[]}
+     */
+    countBits(n: number): number[] {
+        let offset = 1;
+        const dp = new Array(n+1).fill(0);
+        if( n >= 1 ) dp[1] = 1
+        for (let i = 2; i <= n ; i++) {
+            if(i === offset * 2) {
+                dp[i] = 1;
+                offset = offset * 2;
+            } else {
+                dp[i] = 1 + dp[i - offset];
+            }
+        }
+
+        return dp;
+    }
+}
